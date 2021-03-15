0400e433c54f699e410e1b03794edc9bcab46841
Roberto Asiel Guevara Castañeda <roberto.guevara.ext@peoplewalking.com>
2021-03-15 15:21:32 -0400

    Added device funtionalities

--- frontend/src/app/pages/device-details/device-details.component.ts
+++ frontend/src/app/pages/device-details/device-details.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, Subscription } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { Gateway } from '../../interfaces';
+import { BreadcrumbsService } from '../../services/breadcrumbs.service';
+import { DevicesService } from '../../services/devices.service';
+import { GatewaysService } from '../../services/gateways.service';
 
 @Component({
   selector: 'app-device-details',
   templateUrl: './device-details.component.html',
-  styleUrls: ['./device-details.component.scss']
+  styleUrls: ['./device-details.component.scss'],
 })
-export class DeviceDetailsComponent implements OnInit {
+export class DeviceDetailsComponent implements OnInit, OnDestroy {
+  gateway: Gateway | null = null;
+  form: FormGroup = this.builder.group({
+    _id: [''],
+    uid: [''],
+    vendor: [''],
+    status: [''],
+  });
+  subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private deviceService: DevicesService,
+    private gatewayService: GatewaysService,
+    private builder: FormBuilder,
+    private breadcrumbService: BreadcrumbsService,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.breadcrumbService.reset();
+    this.subscriptions.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.breadcrumbService.goTo([
+      { path: '/home', icon: 'home', text: 'Home' },
+      { path: '/devices', icon: 'list', text: 'Devices' },
+      { icon: 'pencil', text: 'Device Details' },
+    ]);
+    const subs = this.route.params
+      .pipe(switchMap(({ id }) => (id === 'new' ? of(null) : this.deviceService.getDeviceDatails(id))))
+      .subscribe({
+        next: (device) => {
+          if (device) {
+            this.form.patchValue(device);
+          }
+        },
+      });
+
+    this.subscriptions.add(subs);
+  }
+
+  saveDevice(): void {
+    const device = this.form.value;
+
+    if (!device._id) {
+      delete device._id;
+    }
+
+    const subs = this.route.params
+      .pipe(
+        switchMap(({ gid }) => (gid ? this.gatewayService.getGatewayDatails(gid) : of(null))),
+        switchMap((gateway) => {
+          if (gateway && !device._id) {
+            this.gateway = gateway;
+            return this.gatewayService.addGatewayDevice(gateway._id, device);
+          }
+          return this.deviceService.updateDevice(device);
+        }),
+      )
+      .subscribe({
+        next: (result) => {
+          if (result) {
+            this.goToList();
+          }
+        },
+      });
+
+    this.subscriptions.add(subs);
+  }
+
+  goToList(): void {
+    this.router.navigate(this.gateway ? ['/gateway', this.gateway._id] : ['/devices']);
+  }
 }
--- frontend/src/app/pages/devices-list/devices-list.component.ts
+++ frontend/src/app/pages/devices-list/devices-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BreadcrumbsService } from '../../services/breadcrumbs.service';
+import { DevicesService } from '../../services/devices.service';
+import { Device } from '../../interfaces';
 
 @Component({
   selector: 'app-devices-list',
   templateUrl: './devices-list.component.html',
-  styleUrls: ['./devices-list.component.scss']
+  styleUrls: ['./devices-list.component.scss'],
 })
-export class DevicesListComponent implements OnInit {
+export class DevicesListComponent implements OnInit, OnDestroy {
+  devices: Device[] = [];
+  subscriptions: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private deviceService: DevicesService, private breadcrumbService: BreadcrumbsService) {}
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.breadcrumbService.reset();
+    this.subscriptions.unsubscribe();
   }
 
+  ngOnInit(): void {
+    const subs = this.deviceService.getDeviceList().subscribe({
+      next: (list) => {
+        this.devices = list;
+      },
+    });
+
+    this.breadcrumbService.goTo([
+      { path: '/home', icon: 'home', text: 'Home' },
+      { icon: 'list', text: 'Devices' },
+    ]);
+
+    this.subscriptions.add(subs);
+  }
 }
